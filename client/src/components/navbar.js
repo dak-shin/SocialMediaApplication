72808a443c274326bd0037cfd61d7174f0c0612b
Dakshin S.A <dakshinsa2107@gmail.com>
2021-04-25 17:05:22 +0530

    Minor changes

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 import { Menu, Segment } from 'semantic-ui-react'
 
 const Navbar = () => {
 
-    const [activeItem, setActiveItem] = useState('');
+    const path = window.location.pathname;
+
+    const pathName = path === '/'? 'home' : path.substr(1);
+
+    const [activeItem, setActiveItem] = useState(pathName);
 
     const handleItemClick = (event,{name}) => {
         
@@ -11,22 +16,28 @@ const Navbar = () => {
     };
     return (
       <div>
-        <Menu pointing secondary>
+        <Menu pointing secondary size="massive">
           <Menu.Item
             name='home'
             active={activeItem === 'home'}
             onClick={handleItemClick}
+            as={Link}
+            to="/"
           />
           <Menu.Menu position='right'>
             <Menu.Item
                 name='login'
                 active={activeItem === 'login'}
                 onClick={handleItemClick}
+                as={Link}
+                to="/login"
              />
             <Menu.Item
               name='register'
               active={activeItem === 'register'}
               onClick={handleItemClick}
+              as={Link}
+                to="/register"
             />
           </Menu.Menu>
         </Menu>
