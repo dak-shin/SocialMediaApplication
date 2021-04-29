e608e1c38af40ccbdb3a7ba46e78033a5c720ee1
dak-shin <dakshinsa2107@gamil.com>
2021-04-30 04:13:43 +0530

    config.js added

---
+++ config.js
@@ -0,0 +1,6 @@
+
+
+const MONGOURL = "mongodb+srv://firstUser:chicken65@cluster0.nbhdh.mongodb.net/Social_Media_Application?retryWrites=true&w=majority";
+const SECRET_KEY = "HELLO DARKNESS MY OLD FIREND";
+
+module.exports = {MONGOURL, SECRET_KEY};
